6d112047091f436e2570a5a54460d99e5c8babe8
Cliff Wulfman <cwulfman@princeton.edu>
2022-03-30 20:14:01 -0400

    Documentation for Issue and Article classes.

--- corpus_generator/issue.py
+++ corpus_generator/issue.py
@@ -1,53 +1,63 @@
-"""A class modeling a newspaper issue in the Daily Princetonian collection."""
+"""A class modeling a newspaper issue in the Daily Princetonian collection.
+
+Newspaper issues in the Daily Princetonian collection are encoded
+as METS/ALTO files in a directory, following some conventions:
+
+The METS file is named Princetonian_YYYY-MM-DD_vNN_nNNN_NNNN-METS.xml
+The ALTO files are stored in a subdirectory called Princetonian-ALTO;
+they are named Princetonian_YYYY-MM-DD_vNN_nNNN_NNNN_ALTONNNN.xml."""
 
 from pathlib import Path
-from  mets import Mets
-from  alto import Alto
+from .mets import Mets
 
 
 class Issue:
-    def __init__(self, path):
-        self._path = Path(path)
+    """Class Issue models a newspaper issue."""
+
+    def __init__(self, resource_path):
+        """Resource_path is the path to the subdirectory
+        containing the METS and ALTO files."""
+        self._path = Path(resource_path)
         self._mets = None
         self._articles = []
 
     @property
     def mets(self):
+        """Instantiate a Mets object if there is none."""
         if not self._mets:
             self._mets = Mets(next(self._path.glob("*METS.xml")))
         return self._mets
 
     @property
     def articles(self):
+        """Instantiate a list of articles if there is none."""
         if not self._articles:
             for item in self.mets.articles:
                 self._articles.append(Article(item, self.mets))
         return self._articles
 
+
 class Article:
+    """A class modeling an article in a METS/ALTO resource."""
     def __init__(self, mets_div, mets):
         self._xml = mets_div
         self.mets = mets
 
     @property
     def title(self):
+        """By convention, the div @LABEL property is the
+        article title."""
         return self._xml.get("LABEL")
 
     @property
     def text(self):
-        areas = self._xml.xpath(".//mets:area", namespaces = Mets.ns_map)
+        """The METS Logical StructMap maps articles to
+        sequences of text blocks in ALTO files."""
+        areas = self._xml.xpath(".//mets:area", namespaces=Mets.ns_map)
         content = []
         for area in areas:
             file_id = area.get("FILEID")
             alto = self.mets.alto_objects[file_id]
             block = alto.text_blocks[area.get("BEGIN")]
-            # content.append(' '.join(list(block.values())))
             content.append(block)
         return content
-
-            
-issue_path = "/Users/cwulfman/repos/github/pulibrary/corpus-generator/tests/data/06_01"
-
-issue = Issue(issue_path)
-article = issue.articles[1]
-    
